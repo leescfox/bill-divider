c3aade59a142b5f980c5d6528783641dcd11cd83
leescfox <nikkartashov740@gmail.com>
2023-12-09 04:36:26 +0300

    ok

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { useBillStore } from '@/store/billStore'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 
-const routes = [
+const routes: RouteRecordRaw[] = [
     {
         name: 'home',
         path: '/',
